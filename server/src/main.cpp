5f3cb6522ff40c69fe19ebb0f9c23b375ca53a49
scala-anon <titanfall2003@gmail.com>
2024-11-12 19:16:50 -0500

    updated pathways in server dir

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,35 +1,46 @@
 #include "ingestion.grpc.pb.h"
-#include "ingestion.pb.h"
 
+#include <grpc++/grpc++.h>
 #include <grpc/grpc.h>
 #include <grpcpp/server_builder.h>
 
 #include <iostream>
+#include <memory>
+#include <string>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using dp::service::ingestion::DpIngestionService;
+using dp::service::ingestion::IngestDataRequest;
+using dp::service::ingestion::IngestDataResponse;
 
 //TODO build method to read in data from michael's data file then when finishing the 
 // ingestion service implementation when giving the server the data it needs to should 
 // be the data that was read in can do a simple loop to read in bytes of data then send them to the server
 
 // potential of what mine should look like
-class IngestionServiceImpl final : public DpIngestionService::ingestion::Service{
+class IngestionServiceImpl final : public DpIngestionService::Service{
     Status ingestData(ServerContext* context, const IngestDataRequest* datarequest, IngestDataResponse* dataresponse) override{
         
         return Status::OK;
     }
 
-    Status ingestData(ServerContext* context, const IngestDataRequest* datarequest, ServerWriter<IngestDataResponse>* writer) override{
+/*    Status ingestData(ServerContext* context, const IngestDataRequest* datarequest, ServerWriter<IngestDataResponse>* writer) override{
         
     }
-}
+*/
+};
 
 
 
-void RunServer(const std::string% db_path){
+void RunServer(){
     std::string server_address("0.0.0.0:50051");
-    IngestionServiceImpl service(db_path);
+    IngestionServiceImpl service;
     
     ServerBuilder builder;
-    builder.AddListeningPort(server_address, groc::InsecureServerCredentials());
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     std::unique_ptr<Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << server_address << std::endl;
